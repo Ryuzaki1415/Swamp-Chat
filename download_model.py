eaf1f3a4e74926ce85a71dd084c7145d42fc07f6
dheeraj <dheerajnunni@gmail.com>
2024-01-20 19:53:22 +0530

    first commit

---
+++ download_model.py
@@ -0,0 +1,7 @@
+# for filename in filenames:
+#         downloaded_model_path = hf_hub_download(
+#                     repo_id=model_id,
+#                     filename=filename,
+#                     token=HUGGING_FACE_API_KEY
+#         )
+#         print(downloaded_model_path)
